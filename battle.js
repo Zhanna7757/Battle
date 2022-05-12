941e2e461519064f5391eb9505041c57c0049232
Zhanna7757 <100212080+Zhanna7757@users.noreply.github.com>
2022-05-13 01:30:30 +0500

    Add files via upload

--- battle.js
+++ battle.js
@@ -1,3 +1,36 @@
+var model = {
+    boardSize: 7,
+    numShips: 10,
+    shipLength: 4,
+    shipsSunk: 0,
+
+    ships: [
+        { locations: [0, 0, 0, 0], hits: ["", "", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0], hits: ["", ""] },
+        { locations: [0, 0], hits: ["", ""] },
+        { locations: [0, 0], hits: ["", ""] },
+        { locations: [0], hits: [""] },
+        { locations: [0], hits: [""] },
+        { locations: [0], hits: [""] },
+        { locations: [0], hits: [""] },
+    ],
+
+}
+
+fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+        var ship = this.ships[i];
+        locations = ship.locations;
+        var index = locations.indexOf(guess);
+        if (index >= 0) {
+
+        }
+    }
+}
+
+
 var view = {
     displayMessage: function (m) {
         var messageArea = document.getElementById("messageArea");
